e49d0f1f214be2dce9a148345adf7286b436942f
mpshimul <35893908+mpshimul@users.noreply.github.com>
2026-05-22 19:03:06 +0600

    Add empty line for better code readability

--- DhakaMovieProvider/src/main/kotlin/com/dhakamovie/DhakaMoviePlugin.kt
+++ DhakaMovieProvider/src/main/kotlin/com/dhakamovie/DhakaMoviePlugin.kt
@@ -3,6 +3,7 @@ package com.dhakamovie
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
+
 @CloudstreamPlugin
 class DhakaMoviePlugin : Plugin() {
     override fun load() {
